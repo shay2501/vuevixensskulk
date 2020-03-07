5a81a894b27d3b646f96fc269752adf865b8e069
Shannon <shannon.whalen@gmail.com>
2020-03-07 13:10:33 -0500

    Chapter 1
    End of Chapter 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <v-app>
-    <main>
-      <div class="app-container">
+    <main :class="{'orange-green': themeSwitched}">
+      <div>
         <header class="app-header dark-brown">
           <h1>My Pet Store</h1>
+          <v-btn @click="themeSwitched = !themeSwitched">Switch theme</v-btn>
         </header>
+
         <div class="wrapper">
           <div class="panel tall-panel light-mint">
             <h2>Pet Products</h2>
@@ -12,7 +14,7 @@
             <p>Kibble, sale in bulk, $20/lb</p>
             <img
               src="https://raw.githubusercontent.com/VueVixens/projects/master/petshop/images/food.png"
-            />
+            >
           </div>
           <div class="panel bisque">
             <h2>Donate</h2>
@@ -22,7 +24,7 @@
             <p>Fisher, Chihuahua, age 3</p>
             <img
               src="https://raw.githubusercontent.com/VueVixens/projects/master/petshop/images/chihuahua.jpg"
-            />
+            >
           </div>
 
           <div class="panel bisque">
@@ -33,14 +35,14 @@
             <p>Meet Stanley, A young French Bulldog</p>
             <img
               src="https://raw.githubusercontent.com/VueVixens/projects/master/petshop/images/bulldog.jpg"
-            />
+            >
           </div>
           <div class="panel tall-panel light-mint">
             <h2>Success Stories</h2>
             <p>Bennie found his forever home!</p>
             <img
               src="https://raw.githubusercontent.com/VueVixens/projects/master/petshop/images/collie.jpg"
-            />
+            >
           </div>
 
           <div class="panel bisque">
@@ -61,104 +63,105 @@
 
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      themeSwitched: true
+    };
   }
 };
 </script>
 
-	<style lang="scss">
-	@import url("https://fonts.googleapis.com/css?family=Roboto");
 
-	/*brown and mint*/
-	/*dark brown 32292F
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Roboto");
+
+/*brown and mint*/
+/*dark brown 32292F
 	light mint 99E1D9
 	bisque F0F7F4
 	dark mint 70ABAF
 	light brown 705D56*/
 
-	*,
-	*:before,
-	*:after {
-	  box-sizing: border-box;
-	}
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
 
-	body {
-	  margin: 0;
-	  padding: 0;
-	}
+body {
+  margin: 0;
+  padding: 0;
+}
 
-	main {
-	  padding: 40px;
-	  font-family: "Roboto", "sans-serif";
-	  background: #fff top center repeat;
-	  color: #444;
-	  background-image: url("https://raw.githubusercontent.com/VueVixens/projects/master/petshop/images/bg.jpg");
-	}
+main {
+  padding: 40px;
+  font-family: "Roboto", "sans-serif";
+  background: #fff top center repeat;
+  color: #444;
+  background-image: url("https://raw.githubusercontent.com/VueVixens/projects/master/petshop/images/bg.jpg");
+}
 
-	h1,
-	p {
-	  margin: 0 0 1em 0;
-	}
+h1,
+p {
+  margin: 0 0 1em 0;
+}
 
-	img {
-	  max-width: 100%;
-	  display: block;
-	  margin: 0 auto;
-	}
+img {
+  max-width: 100%;
+  display: block;
+  margin: 0 auto;
+}
 
-	.app-container {
-	  max-width: 940px;
-	  margin: 0 auto;
-	  background-color: #fff;
-	}
+.app-container {
+  max-width: 940px;
+  margin: 0 auto;
+  background-color: #fff;
+}
 
-	.app-container > * {
-	  border-radius: 5px;
-	  font-size: 150%;
-	  margin-bottom: 10px;
-	}
+.app-container > * {
+  border-radius: 5px;
+  font-size: 150%;
+  margin-bottom: 10px;
+}
 
-	.wrapper {
-	  display: grid;
-	  grid-gap: 10px;
-	  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-	  grid-auto-rows: minmax(150px, auto);
-	}
+.wrapper {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-auto-rows: minmax(150px, auto);
+}
 
-	.wrapper > * {
-	  padding: 15px;
-	  border-radius: 5px;
-	}
+.wrapper > * {
+  padding: 15px;
+  border-radius: 5px;
+}
 
-	.light-mint {
-	  background-color: #99e1d9;
-	}
+.light-mint {
+  background-color: #99e1d9;
+}
 
-	.dark-mint {
-	  background-color: #70abaf;
-	}
+.dark-mint {
+  background-color: #70abaf;
+}
 
-	.light-brown {
-	  background-color: #705d56;
-	  color: #f0f7f4;
-	}
+.light-brown {
+  background-color: #705d56;
+  color: #f0f7f4;
+}
 
-	.dark-brown {
-	  background-color: #32292f;
-	  color: #f0f7f4;
-	}
+.dark-brown {
+  background-color: #32292f;
+  color: #f0f7f4;
+}
 
-	.bisque {
-	  background-color: #f0f7f4;
-	}
+.bisque {
+  background-color: #f0f7f4;
+}
 
-	/*orange and green*/
-	/*
+/*orange and green*/
+/*
 	dark orange 771100
 	orange CC6633
 	light orange FF9900
@@ -166,52 +169,52 @@ export default {
 	light green 86a193
 	*/
 
-	.orange-green {
-	  background-image: url("https://raw.githubusercontent.com/VueVixens/projects/master/petshop/images/bg2.jpg");
-	  .light-mint {
-	    background-color: #86a193;
-	  }
+.orange-green {
+  background-image: url("https://raw.githubusercontent.com/VueVixens/projects/master/petshop/images/bg2.jpg");
+  .light-mint {
+    background-color: #86a193;
+  }
 
-	  .dark-mint {
-	    background-color: #689980;
-	  }
+  .dark-mint {
+    background-color: #689980;
+  }
 
-	  .light-brown {
-	    background-color: #cc6633;
-	  }
+  .light-brown {
+    background-color: #cc6633;
+  }
 
-	  .dark-brown {
-	    background-color: #771100;
-	  }
+  .dark-brown {
+    background-color: #771100;
+  }
 
-	  .bisque {
-	    background-color: #ff9900;
-	  }
-	}
+  .bisque {
+    background-color: #ff9900;
+  }
+}
 
-	.panel {
-	  /* needed for the flex layout*/
-	  margin-left: 5px;
-	  margin-right: 5px;
-	  flex: 1 1 200px;
-	}
+.panel {
+  /* needed for the flex layout*/
+  margin-left: 5px;
+  margin-right: 5px;
+  flex: 1 1 200px;
+}
 
-	.tall-panel {
-	  grid-row-end: span 2;
-	}
+.tall-panel {
+  grid-row-end: span 2;
+}
 
-	.app-header,
-	.app-footer {
-	  flex: 0 1 100%;
-	  padding: 15px;
-	  text-align: center;
-	}
+.app-header,
+.app-footer {
+  flex: 0 1 100%;
+  padding: 15px;
+  text-align: center;
+}
 
-	/* We need to set the margin used on flex items to 0 as we have gaps in grid.  */
-	@supports (display: grid) {
-	  .wrapper > * {
-	    margin: 0;
-	  }
-	}
-	</style>
+/* We need to set the margin used on flex items to 0 as we have gaps in grid.  */
+@supports (display: grid) {
+  .wrapper > * {
+    margin: 0;
+  }
+}
+</style>
 
