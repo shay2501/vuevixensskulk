133a7d24238dfba9bcc1c36156e96dc230016ce3
Shannon <shannon.whalen@gmail.com>
2020-03-07 14:17:04 -0500

    Chapter 2
    End of chapter 2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Home from "./views/Home";
+import Pets from "./views/Pets";
+
 import vuetify from "@/plugins/vuetify";
+import VueRouter from "vue-router";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: Home
+  },
+  {
+    path: "/pets",
+    component: Pets
+  }
+];
+
+const router = new VueRouter({ routes });
+
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount("#app");
