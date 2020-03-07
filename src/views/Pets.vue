72c6b42b1473b1c5dab29e9136082458349b4644
Shannon <shannon.whalen@gmail.com>
2020-03-07 15:01:02 -0500

    Chapter 3
    End of chapter 3

--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -11,6 +11,9 @@
 <script>
 import { Dogs } from "../data/dogs";
 import Dog from "../components/Dog.vue";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://dog.ceo/api";
 
 export default {
   components: {
@@ -20,6 +23,21 @@ export default {
     return {
       dogs: Dogs
     };
+  },
+  created() {
+    this.dogs.forEach(dog => {
+      dog.img = "";
+    });
+    const linksArray = this.dogs.map(
+      dog => "/breed/" + dog.breed + "/images/random"
+    );
+    axios.all(linksArray.map(link => axios.get(link))).then(
+      axios.spread((...res) => {
+        this.dogs.forEach((dog, index) => {
+          dog.img = res[index].data.message;
+        });
+      })
+    );
   }
 };
 </script>
