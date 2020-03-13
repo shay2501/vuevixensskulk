3c252e27270c0c111e6024dc4fa55e2cbf6bd197
Shannon <shannon.whalen@gmail.com>
2020-03-12 22:05:10 -0400

    Form submit

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,9 +1,47 @@
 <template>
-  <div class="form-wrapper">This form works!</div>
+  <div class="form-wrapper">
+    <v-form>
+      <div class="form-wrapper">
+        <v-form>
+          <v-text-field label="Name" v-model="name"></v-text-field>
+          <v-text-field label="Email" v-model="email"></v-text-field>
+          <v-text-field label="Phone" v-model="phone"></v-text-field>
+          <v-btn @click="submit">Submit</v-btn>
+        </v-form>
+      </div>
+    </v-form>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      phone: ""
+    };
+  },
+  methods: {
+    submit() {
+      console.log(
+        "Name:",
+        this.name,
+        "Email:",
+        this.email,
+        "Phone:",
+        this.phone
+      );
+    }
+  }
+};
+</script>
+
+
 
 <style scoped>
 .form-wrapper {
   padding: 40px;
+  text-align: center;
 }
 </style>
